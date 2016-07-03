7c55975922354880f4d8ba058b06be96922ff968
bud aminof <budaminof@gmail.com>
2016-07-03 13:31:58 -0600

    added a time stamp to pomodoro table

--- migrations/20160701174645_pomodoros.js
+++ migrations/20160701174645_pomodoros.js
@@ -1,9 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('pomodoro', function(table){
+  return knex.schema.createTable('pomodoro', function(table) {
     table.increments('id');
     table.string('name').notNullable();
     table.integer('user_id').references('users.id').onDelete('CASCADE').onUpdate('CASCADE');
+    table.timestamp('created_at').notNullable().defaultTo(knex.raw('now()'));
   })
 
 };
