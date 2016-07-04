e91e7146ff2ff5653392dd2668ffddfb41cc5c10
bud aminof <budaminof@gmail.com>
2016-07-04 17:09:15 -0600

    can send back to client a lit of done pomodoros according to phone id

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,6 @@ var knex = require('knex')(require('../knexfile')[process.env.DB_ENV || 'develop
 
 router.post('/', function(req, res, next) {
   console.log("**********************************************************************");
-  console.log("in service");
-  console.log(req.body);
   knex('users')
     .where({phone_id: req.body.phone_id})
     .then(function (user) {
@@ -49,7 +47,7 @@ router.post('/', function(req, res, next) {
 
 
 router.get('/:phoneId', function(req, res, next){
-  console.log("************************************************");
+  console.log("********************");
   knex('users')
   .where({phone_id: req.params.phoneId})
   .innerJoin('pomodoro', 'pomodoro.user_id', 'users.id')
