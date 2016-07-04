aa387748dbd0c0218179f623eacd6eddaa8dd33e
bud aminof <budaminof@gmail.com>
2016-07-04 11:34:14 -0600

    added user get request

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var knex = require('knex')(require('../knexfile')[process.env.DB_ENV || 'development']);
 
 router.post('/', function(req, res, next) {
+  console.log("**********************************************************************");
+  console.log("in service");
+  console.log(req.body);
   knex('users')
     .where({phone_id: req.body.phone_id})
     .then(function (user) {
@@ -44,4 +47,15 @@ router.post('/', function(req, res, next) {
   })
 });
 
+
+router.get('/:phoneId', function(req, res, next){
+  console.log("************************************************");
+  knex('users')
+  .where({phone_id: req.params.phone_id})
+  .innerJoin('pomodoro', 'pomodoro.user_id', 'users.id')
+  .then(function(res){
+    console.log(res);
+    return res.status(200).json(res[0]);
+  })
+})
 module.exports = router;
